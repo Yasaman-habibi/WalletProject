74d66d025f302f445e9fd8bd5865f737deba27f7
Yasaman-habibi <yasihabibi833@gmail.com>
2024-10-21 13:46:57 +0330

    Update IBANValidator.java

--- src/main/java/ir/freeland/springboot/entity/IBANValidator.java
+++ src/main/java/ir/freeland/springboot/entity/IBANValidator.java
@@ -1,4 +1,4 @@
-package ir.freeland.springboot.persistence.model;
+package ir.freeland.springboot.entity;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.regex.Pattern;
