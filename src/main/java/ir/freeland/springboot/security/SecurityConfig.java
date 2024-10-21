55a6c581f558a28826609a5d53193af9f7a9b40e
Yasaman-habibi <yasihabibi833@gmail.com>
2024-10-21 13:54:02 +0330

    Update SecurityConfig.java

--- src/main/java/ir/freeland/springboot/security/SecurityConfig.java
+++ src/main/java/ir/freeland/springboot/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package ir.freeland.springboot.config;
+package ir.freeland.springboot.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
