7620fdc375b463393f0948012e2295a5b48a6d7b
Yasaman-habibi <yasihabibi833@gmail.com>
2024-10-21 13:50:18 +0330

    Update ErrorResponseService.java

--- src/main/java/ir/freeland/springboot/exception/ErrorResponseService.java
+++ src/main/java/ir/freeland/springboot/exception/ErrorResponseService.java
@@ -1,4 +1,4 @@
-package ir.freeland.springboot.web.exception;
+package ir.freeland.springboot.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
