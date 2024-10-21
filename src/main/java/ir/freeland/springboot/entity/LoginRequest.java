11f6986e23e6bfa8c54e767309a473c9f797b2b8
Yasaman-habibi <yasihabibi833@gmail.com>
2024-10-21 13:48:17 +0330

    Update LoginRequest.java

--- src/main/java/ir/freeland/springboot/entity/LoginRequest.java
+++ src/main/java/ir/freeland/springboot/entity/LoginRequest.java
@@ -1,4 +1,4 @@
-package ir.freeland.springboot.persistence.model;
+package ir.freeland.springboot.entity;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
